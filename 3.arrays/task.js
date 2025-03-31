01a05e2722958a18a25cc998bcf083255fb62e55
Alina <eparis@atlassian.com>
2025-03-31 14:58:55 +0300

    third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	const newList = users.filter(user => user.gender === gender);
+
+
+	const averageAge = newList.reduce((sum, user) => sum + user.age, 0) / newList.length;
+
+	return averageAge;
 }
+  
